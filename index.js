dfa973f638b0a2d429d4c51d2e69d2325cd239e8
jamesburns22 <james@thedogslanding.com>
2022-07-17 16:20:42 +0300

    v0.3.4 -- addedlower/uppercase

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const wordNumber = document.getElementById("word-counter");
 const textInput = document.getElementById("text-area");
 const showIfEbru = document.getElementById("show-if-ebru");
 const showIfPiglet = document.getElementById("show-if-piglet");
+const upperCaseText = document.getElementById("text-area-uppercaser");
+const lowerCaseText = document.getElementById("text-area-lowercaser");
 
 function countUp() {
     messageLength.innerHTML = textInput.value.length
@@ -20,30 +22,40 @@ function wordCounter() {
     wordNumber.innerHTML = (`Words: ${wordAmountFiltered.length}`)
 }
 
-
-const checkIfEbru = () => {
-    const mainMessageLower = textInput.value.toLowerCase();
-    if (mainMessageLower === "my name is ebru") {
-        showIfEbru.classList.replace("ebru-false", "ebru-true")
-        showIfEbru.hidden = false;
-    } else (
-        showIfEbru.classList.replace("ebru-true", "ebru-false")
-    );
+function makeTextLowerCase() {
+    lowerCaseText.value = textInput.value.toLowerCase();
 }
 
-const checkIfPiglet = () => {
-    const mainMessageLower = textInput.value.toLowerCase()
-    if (mainMessageLower === "my name is piglet") {
-        showIfPiglet.classList.replace("piglet-false", "piglet-true")
-        showIfEbru.hidden = true;
-
-    } else (
-        showIfPiglet.classList.replace("piglet-true", "piglet-false")
-    );
+function makeTextUpperCase() {
+    upperCaseText.value = textInput.value.toUpperCase();
 }
+
+
+// const checkIfEbru = () => {
+//     const mainMessageLower = textInput.value.toLowerCase();
+//     if (mainMessageLower === "my name is ebru") {
+//         showIfEbru.classList.replace("ebru-false", "ebru-true")
+//         showIfEbru.hidden = false;
+//     } else (
+//         showIfEbru.classList.replace("ebru-true", "ebru-false")
+//     );
+// }
+
+// const checkIfPiglet = () => {
+//     const mainMessageLower = textInput.value.toLowerCase()
+//     if (mainMessageLower === "my name is piglet") {
+//         showIfPiglet.classList.replace("piglet-false", "piglet-true")
+//         showIfEbru.hidden = true;
+
+//     } else (
+//         showIfPiglet.classList.replace("piglet-true", "piglet-false")
+//     );
+// }
 
 textInput.addEventListener("input", countUp);
 textInput.addEventListener("input", countDown);
 textInput.addEventListener("input", wordCounter);
-textInput.addEventListener("input", checkIfEbru);
-textInput.addEventListener("input", checkIfPiglet);
+textInput.addEventListener("input", makeTextLowerCase);
+textInput.addEventListener("input", makeTextUpperCase);
+// textInput.addEventListener("input", checkIfEbru);
+// textInput.addEventListener("input", checkIfPiglet);
